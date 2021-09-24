a8e776d6dd6c848db864a486fd0bf76d542c6377
VictorCV <alucan0184@ieselcaminas.org>
2021-09-25 01:10:37 +0200

    Proyectos IntelliJ (Para ej1-AD)-Naveg dir

--- src/exercicis/Exercici_1.kt
+++ src/exercicis/Exercici_1.kt
@@ -7,14 +7,16 @@ import kotlin.math.absoluteValue
 
 
 fun main(args: Array<String>) {
-    val ent = File.listRoots()[0]
-    val f = ent
+    val f = File.listRoots()[0]
 
     llistaDirectori(f)
 }
 
 fun llistaDirectori(f: File) {
-    val s = "Llista de fitxers i directoris del directori " + f.getCanonicalPath()
+
+//    1R LISTADO DE DIRECTORIOS------------------------------------
+
+    val s = "Llista de fitxers i directoris del directori " + f
     println(s)
     println("-".repeat(s.length))
 
@@ -22,7 +24,9 @@ fun llistaDirectori(f: File) {
 
     println("0" + ".- " + "Directori Pare")
 
-    for (e in f.listFiles().sorted()) {
+    val arr1 = f.listFiles().sorted()
+
+    for (e in  arr1){
 
         if (e.isFile())
             println(count.toString() + ".- " + e.getName() + "\t " + e.length())
@@ -32,6 +36,8 @@ fun llistaDirectori(f: File) {
         count = count + 1;
 
     }
+
+    //1A INTRODUCCION DE DIRECTORIO-----------------------------------------------------
 
     val v = 0
 
@@ -48,55 +54,170 @@ fun llistaDirectori(f: File) {
 //
 //            }else
 //                println("El valor no es numeric i no es valid")
+
             if (v == -1) {
-                llistaDirectori(f)
+                    if ( f.canonicalPath != "/"){
+                        val dirPadre= f.parentFile
+                        if (dirPadre.exists()) {
+                            llistaDirectori(dirPadre)
+                    }
+                }
             }else
 
-            if (v<=25) {
-                val peticion = File(f.listFiles().sorted()[v].name)
+                if (v<=25) {
+                    val peticion = File(f.listFiles().sorted()[v].canonicalPath)
 
-                println(peticion)
-
-                val entrada = "/" + peticion.toString()
-                val arch = File(entrada)
-                if (arch.exists()) {
-                    if (arch.isDirectory()) {
-                        if (arch.canRead()) {
-                            seleccionaDir(arch)
+                    val entrada = peticion.toString()
+                    val arch = File(entrada)
+                    if (arch.exists()) {
+                        if (arch.isDirectory()) {
+                            if (arch.canRead()) {
+                                seleccionaDir(arch)
+                            } else
+                                println("\nEl usuari no te permisos de lectura sobre el directori")
                         } else
-                            println("\nEl usuari no te permisos de lectura sobre el directori")
-                    } else
 
-                        println("\nNo és un directori")
-                } else
-                    println("\nNo existeix el directori")
-            }else
-                println("\nIntrodueix un valor en el rang")
+                            println("\nNo és un directori")
+                    } else
+                        println("\nNo existeix el directori")
+                }else
+                    println("\nIntrodueix un valor en el rang")
         }
     }while (v != -2)
-//    if (v == -2){
-//        println("Fin del programa")
-//    }
 }
 
-    fun seleccionaDir(arch: File) {
-        val s = "Llista de fitxers i directoris del directori " + arch.getCanonicalPath()
-        println(s)
-        println("-".repeat(s.length))
+fun seleccionaDir(arch: File) {
+    val s = "Llista de fitxers i directoris del directori " + arch.getCanonicalPath()
+    println(s)
+    println("-".repeat(s.length))
 
-        var count = 1
+    var count = 1
 
 
-        println("0" + ".- " + "Directori Pare")
+    println("0" + ".- " + "Directori Pare")
 
-        for (e in arch.listFiles().sorted()) {
+    val arr2 = arch.listFiles().sorted()
 
-            if (e.isFile())
-                println(count.toString() + ".- " + e.getName() + "\t " + e.length())
-            if (e.isDirectory())
-                println(count.toString() + ".- " + e.getName() + "\t <Directori>")
+    if (arch.isDirectory){
+    println(arch)
 
-            count = count + 1;
-
-        }
+    llistaDirectori(arch)
     }
+
+    for (e in arr2) {
+
+        if (e.isFile())
+            println(count.toString() + ".- " + e.getName() + "\t " + e.length())
+        if (e.isDirectory())
+            println(count.toString() + ".- " + e.getName() + "\t <Directori>")
+
+        count = count + 1;
+
+    }
+
+}
+
+//ANTERIOR A NAVEGACION LIBRE
+//
+//package exercicis
+//
+//import java.io.File
+//import java.io.BufferedReader
+//import java.io.InputStreamReader
+//import kotlin.math.absoluteValue
+//
+//
+//fun main(args: Array<String>) {
+//    val ent = File.listRoots()[0]
+//    val f = ent
+//
+//    llistaDirectori(f)
+//}
+//
+//fun llistaDirectori(f: File) {
+//    val s = "Llista de fitxers i directoris del directori " + f.getCanonicalPath()
+//    println(s)
+//    println("-".repeat(s.length))
+//
+//    var count = 1
+//
+//    println("0" + ".- " + "Directori Pare")
+//
+//    for (e in f.listFiles().sorted()) {
+//
+//        if (e.isFile())
+//            println(count.toString() + ".- " + e.getName() + "\t " + e.length())
+//        if (e.isDirectory())
+//            println(count.toString() + ".- " + e.getName() + "\t <Directori>")
+//
+//        count = count + 1;
+//
+//    }
+//
+//    val v = 0
+//
+//    do {
+//        println("\nIntrodueix un numero (-1 para acabar):")
+//        val ent = BufferedReader(InputStreamReader(System.`in`)).readLine()
+//        val valor  = File(ent).toString()
+//
+//        if (valor is String) {
+//
+//            val v = valor.toInt() -1
+//
+////            if (v.isnumeric){
+////
+////            }else
+////                println("El valor no es numeric i no es valid")
+//            if (v == -1) {
+//                llistaDirectori(f)
+//            }else
+//
+//                if (v<=25) {
+//                    val peticion = File(f.listFiles().sorted()[v].name)
+//
+//                    println(peticion)
+//
+//                    val entrada = "/" + peticion.toString()
+//                    val arch = File(entrada)
+//                    if (arch.exists()) {
+//                        if (arch.isDirectory()) {
+//                            if (arch.canRead()) {
+//                                seleccionaDir(arch)
+//                            } else
+//                                println("\nEl usuari no te permisos de lectura sobre el directori")
+//                        } else
+//
+//                            println("\nNo és un directori")
+//                    } else
+//                        println("\nNo existeix el directori")
+//                }else
+//                    println("\nIntrodueix un valor en el rang")
+//        }
+//    }while (v != -2)
+////    if (v == -2){
+////        println("Fin del programa")
+////    }
+//}
+//
+//fun seleccionaDir(arch: File) {
+//    val s = "Llista de fitxers i directoris del directori " + arch.getCanonicalPath()
+//    println(s)
+//    println("-".repeat(s.length))
+//
+//    var count = 1
+//
+//
+//    println("0" + ".- " + "Directori Pare")
+//
+//    for (e in arch.listFiles().sorted()) {
+//
+//        if (e.isFile())
+//            println(count.toString() + ".- " + e.getName() + "\t " + e.length())
+//        if (e.isDirectory())
+//            println(count.toString() + ".- " + e.getName() + "\t <Directori>")
+//
+//        count = count + 1;
+//
+//    }
+//}
